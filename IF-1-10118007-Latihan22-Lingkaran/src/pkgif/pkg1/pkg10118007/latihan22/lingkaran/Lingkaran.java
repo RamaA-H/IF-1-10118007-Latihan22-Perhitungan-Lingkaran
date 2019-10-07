fbbc9f0210260425c64603b44e84ffad343839cb
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-10-07 20:21:34 +0700

    Add files via upload

---
+++ IF-1-10118007-Latihan22-Lingkaran/src/pkgif/pkg1/pkg10118007/latihan22/lingkaran/Lingkaran.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg1.pkg10118007.latihan22.lingkaran;
+import java.util.Scanner;
+
+/**
+ * Nama      : Rama Al Halik
+ * Kelas     : IF-1
+ * NIM       : 10118007
+ * Deskripsi : Menghitung Lingkaran
+ * @author Rama
+ */
+public class Lingkaran {
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double d,r, luas,keliling;
+        System.out.println("Perhitungan lingkaran");
+        System.out.println("=================================");
+        
+        Scanner scanner;
+        do{
+            System.out.println("Diamter harus berupa angka!!");
+            System.out.print("Diameter\t: ");
+            System.out.println("");
+            scanner = new Scanner(System.in);
+            
+            
+            
+        }while(!scanner.hasNextDouble());
+        d = scanner.nextDouble();
+        r = d * 0.5;
+        luas = Math.PI * r * r;
+        keliling = d * Math.PI;
+         System.out.println("Hasil");
+            System.out.println("=================================");
+            System.out.printf("Jari jari\t: %,12.2f cm%n" ,r);
+            System.out.printf("Luas\t\t: %,12.2f cm%n" ,luas);
+            System.out.printf("Keliling\t: %,12.2f cm%n" ,keliling);
+    }
+    
+}
